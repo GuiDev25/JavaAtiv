62bc5aafce6b69812e8b6ee27f57a670c3732448
Pedro Guilherme <42542436+pedroguirdc@users.noreply.github.com>
2018-09-21 22:17:49 -0300

    Add files via upload

--- matrizera.java
+++ matrizera.java
@@ -5,17 +5,24 @@
  */
 package guilherme;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Roberta
  */
 public class matrizera extends javax.swing.JFrame {
-String matriz [][]=new String [3][3];
+    String matriz [][]=new String [4][3];
+    boolean enc = false;
+    DefaultTableModel modelo = new DefaultTableModel();
     /**
      * Creates new form matrizera
      */
     public matrizera() {
         initComponents();
+        modelo.addColumn("Cód");
+        modelo.addColumn("Nome");
+        modelo.addColumn("Preço");
         
         matriz [0][0] = "321";
         matriz [0][1] = "Caderno";
@@ -29,6 +36,10 @@ String matriz [][]=new String [3][3];
         matriz [2][1] = "Estojo";
         matriz [2][2] = "R$:10.00";
         
+        matriz [3][0] = "846";
+        matriz [3][1] = "Caneta";
+        matriz [3][2] = "R$:2.00";
+        
     }
                                
     /**
@@ -41,71 +52,80 @@ String matriz [][]=new String [3][3];
     private void initComponents() {
 
         codigo = new javax.swing.JTextField();
-        bt_procurar = new javax.swing.JButton();
-        nome = new javax.swing.JLabel();
         preço = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        nome = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabela = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        bt_procurar.setText("Procurar");
-        bt_procurar.addActionListener(new java.awt.event.ActionListener() {
+        codigo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bt_procurarActionPerformed(evt);
+                codigoActionPerformed(evt);
             }
         });
 
         preço.setText("          ");
 
-        jLabel1.setText("PAPELARIA");
+        tabela.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(tabela);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(codigo, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(161, 161, 161)
-                        .addComponent(bt_procurar))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(119, 119, 119)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(preço, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(nome, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(codigo, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(173, 173, 173)
-                        .addComponent(jLabel1)))
-                .addContainerGap(116, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(nome, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(preço, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addGap(26, 26, 26)
-                .addComponent(nome)
-                .addGap(18, 18, 18)
-                .addComponent(preço)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 123, Short.MAX_VALUE)
+                .addGap(14, 14, 14)
                 .addComponent(codigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(bt_procurar)
-                .addGap(56, 56, 56))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(nome)
+                        .addGap(18, 18, 18)
+                        .addComponent(preço))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(118, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void bt_procurarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_procurarActionPerformed
+    private void codigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_codigoActionPerformed
         for (int i = 0; i < matriz.length; i++) {
-            if (codigo.getText().equals(matriz[i][0])) {
+            if (matriz[i][0].equals(codigo.getText())) {
                 nome.setText(matriz[i][1]);
                 preço.setText(matriz[i][2]);
+                enc = true;
+                modelo.addRow(new String[]{matriz[i][0],matriz[i][1],matriz[i][2]});
+                tabela.setModel(modelo);
+                codigo.setText("");
+                break;
             }
         }
-    }//GEN-LAST:event_bt_procurarActionPerformed
+    }//GEN-LAST:event_codigoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -143,10 +163,10 @@ String matriz [][]=new String [3][3];
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton bt_procurar;
     private javax.swing.JTextField codigo;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel nome;
     private javax.swing.JLabel preço;
+    private javax.swing.JTable tabela;
     // End of variables declaration//GEN-END:variables
 }
